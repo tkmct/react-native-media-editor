413292cba219d2432a78d39ed7f49ae7bb02659a
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2016-12-21 19:21:14 +0900

    Add react-native-image-picker

--- example/index.ios.js
+++ example/index.ios.js
@@ -1,36 +1,93 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+* Sample React Native App
+* https://github.com/facebook/react-native
+* @flow
+*/
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button,
+  Platform
 } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 import RNMediaEditor from 'react-native-media-editor';
 
+
+var options = {
+  title: 'Select Avatar',
+  customButtons: [
+    {name: 'fb', title: 'Choose Photo from Facebook'},
+  ],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images'
+  }
+};
+
+/**
+* The first arg is the options object for customization (it can also be null or omitted for default options),
+* The second arg is the callback which sends object: response (more info below in README)
+*/
+
 export default class example extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photo: null,
+      video: null,
+    };
+
+    this.onButtonPress = this.onButtonPress.bind(this);
+  }
+
   componentWillMount() {
     RNMediaEditor.echo("Hello RNMediaEditor");
+  }
+
+  onButtonPress() {
+    RNMediaEditor.echo("Button pressed");
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        // You can display the image using either data...
+        const source = {uri: 'data:image/jpeg;base64,' + response.data, isStatic: true};
+
+        // or a reference to the platform specific asset location
+        if (Platform.OS === 'ios') {
+          const source = {uri: response.uri.replace('file://', ''), isStatic: true};
+        } else {
+          const source = {uri: response.uri, isStatic: true};
+        }
+
+        this.setState({
+          photo: source
+        });
+      }
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Button
+          onPress={this.onButtonPress}
+          title="Pick Image"
+        />
       </View>
     );
   }
