295ab949dc84aa4f83b478b3a741c90d1a3626ed
Takamichi-tsutsumi <takamichi_tsutsumi@waku-2.com>
2017-02-09 00:24:25 +0900

    update for example

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -4,8 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.rnmediaeditor.RNMediaEditorPackage;
 import com.brentvatne.react.ReactVideoPackage;
+import com.rnmediaeditor.RNMediaEditorPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -29,8 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNMediaEditorPackage(),
             new ReactVideoPackage(),
+            new RNMediaEditorPackage(),
             new ImagePickerPackage(),
             new RCTCameraPackage()
       );
